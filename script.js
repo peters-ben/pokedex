bca5a044bbeb27f39439c0e9eda9d8443ce7b3f9
Ben Peters <bspe227@uky.edu>
2020-06-24 18:56:41 -0500

    Implemented fetch on Pokemon API, basic display of name, type, and weight

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function submit() {
     let pokemon = document.getElementById("search").value;
-    alert(pokemon);
+    pokemon = pokemon.toLowerCase();
+    if(pokemon == "") {
+
+    } else {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon).then(res => {
+        if(res.ok) {
+            console.log("Success!");
+            return res.json();
+        } else {
+            console.log("Error!");
+        }
+
+}).then(data => {
+    document.getElementById("pokemon-name").innerText = "Name: " + data.forms[0].name.slice(0,1).toUpperCase() +
+        data.forms[0].name.slice(1);
+    document.getElementById("pokemon-type").innerText = "Type: " + data.types[0].type.name.slice(0,1).toUpperCase() +
+        data.types[0].type.name.slice(1);
+    document.getElementById("pokemon-weight").innerText = "Weight: " + data.weight + " kg";
+})
+.catch(error => console.log("ERROR"));
+    }
 }

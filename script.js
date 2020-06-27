1d4e73a0e5fdbe2b03ccb35277ffd436066ed3c1
Ben Peters <bspe227@uky.edu>
2020-06-27 18:27:29 -0500

    Added unknown entry for if pokemon doesn't exist, basic picture of pokemon now displayed on entry

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function submit() {
             console.log("Success!");
             return res.json();
         } else {
+            document.getElementById("pokemon-name").innerText = "Name: Unknown Pokemon";
+            document.getElementById("pokemon-type").innerText = "Type: N/A"
+            document.getElementById("pokemon-weight").innerText = "Weight: N/A";
+            document.getElementById("pokemon-img").src = "https://w7.pngwing.com/pngs/190/415/png-transparent-pokemon-gold-and-silver-pokemon-heartgold-and-soulsilver-pokemon-ruby-and-sapphire-question-mark-text-trademark-logo.png";
+            document.getElementById("pokemon-img").height = "100"; !important
             console.log("Error!");
         }
 
@@ -17,7 +22,8 @@ function submit() {
         data.forms[0].name.slice(1);
     document.getElementById("pokemon-type").innerText = "Type: " + data.types[0].type.name.slice(0,1).toUpperCase() +
         data.types[0].type.name.slice(1);
-    document.getElementById("pokemon-weight").innerText = "Weight: " + data.weight + " kg";
+    document.getElementById("pokemon-weight").innerText = "Weight: " + (data.weight / 10) + " kg";
+    document.getElementById("pokemon-img").src = data.sprites.front_default;
 })
 .catch(error => console.log("ERROR"));
     }
